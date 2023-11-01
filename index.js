770178b17a7a56a0f28706c361e76e7ca24e9cc1
aldoresendiz <aldoresendiz@gmail.com>
2023-11-01 12:31:20 -0500

    Adding 404 page view

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    // res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).render('error404')
 })
 // LISTEN
 app.listen(process.env.PORT);
