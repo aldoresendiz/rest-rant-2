ec0e1cf01e2f1d13f1cca16103c4ce18ddf4b3f2
aldoresendiz <aldoresendiz@gmail.com>
2023-11-01 12:25:16 -0500

    Adding home page view

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const express = require('express')
 // CONFIGURATION
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 //ROUTES
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    //res.send('Hello World')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
