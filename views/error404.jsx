92679ce0a6571ecfef600537ffa4e1e4314afe66
aldoresendiz <aldoresendiz@gmail.com>
2023-11-01 14:41:53 -0500

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,14 @@ function error404() {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops! Sorry, We cannot find such page!</p>
+                <div>
+                    <img src="/images/error404.jpg" alt="Error 404" height="300" width="500"></img>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@leekos?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Kostiantyn Li</a> on <a href="https://unsplash.com/photos/a-broken-cell-phone-with-the-word-error-on-it-Fi_nhg5itCw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+                    </div>
+                </div>
+
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/chia-fruit-drink.jpg" alt="Chica Fruit Shake" height="300" width="500"></img>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@cravethebenefits?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Brenda Godinez</a> on <a href="https://unsplash.com/photos/two-fruit-beverages-on-glass-cups-MsTOg6rhRVk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
