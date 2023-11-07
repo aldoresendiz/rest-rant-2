faa7a46a8f8d9665e29970390d827c22602dcf47
aldoresendiz <aldoresendiz@gmail.com>
2023-11-07 17:40:01 -0600

    Validation errors added

--- controllers/places.js
+++ controllers/places.js
@@ -21,8 +21,19 @@ router.post('/', (req, res) => {
             res.redirect('/places')
         })
         .catch(err => {
-            console.log('err', err)
-            res.render('error404')
+            if (err && err.name == 'ValidationError') {
+                let message = 'Validation Error: '
+                for (var field in err.errors) {
+                    message += `${field} was ${err.errors[field].value}.`
+                    message += `${err.errors[field].message}`
+                }
+                console.log('Validation error message: ', message)
+                // TODO: Generar mensahe de error
+                res.render('places/new', {message})
+            }
+            else {
+                res.render('error404')
+            }
         })
 
     // res.send('POST /places stub')
