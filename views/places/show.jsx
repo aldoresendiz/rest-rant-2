130662195e4f6309464aa5d818bdc30951a4a6fd
aldoresendiz <aldoresendiz@gmail.com>
2023-11-07 17:15:31 -0600

    Adding methods to the schemaPlace class

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,22 +5,71 @@ function show(data) {
     return (
         <Def>
             <main>
-                <h1>{data.place.name}</h1>
-                <div>Coming soon!</div>
-                <p>{data.place.city}, { data.place.state}</p>
-                <div>
-                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                        Edit
-                    </a>
-                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                        <button type="submit" className="btn btn-danger">
-                            Delete
-                        </button>
-                    </form>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <img src={data.place.pic} alt={data.place.name} width="200px" height="200px"></img>
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
+                    <div className="col-sm-6">
+                        <h1>{data.place.name}</h1>
+                        <h2>
+                            Rating
+                        </h2>
+                        <h3>
+                            Not Rated
+                        </h3>
+                        <p />
+                        <h2>
+                            Description
+                        </h2>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                        <div className="col-sm-6">
+                            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                                Edit
+                            </a>
+                        </div>
+                        <div className="col-sm-6">
+                            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                                <button type="submit" className="btn btn-danger">
+                                    Delete
+                                </button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
+                <div>Comments</div>
             </main>
         </Def>
     )
 }
+
+//function show(data) {
+//    return (
+//        <Def>
+//            <main>
+//                <h1>{data.place.name}</h1>
+//                <div>Coming soon!</div>
+//                <p>{data.place.city}, {data.place.state}</p>
+//                <div>
+//                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+//                        Edit
+//                    </a>
+//                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+//                        <button type="submit" className="btn btn-danger">
+//                            Delete
+//                        </button>
+//                    </form>
+//                </div>
+//            </main>
+//        </Def>
+//    )
+//}
 
 module.exports = show
